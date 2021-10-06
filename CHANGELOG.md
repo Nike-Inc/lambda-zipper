78046ca2f6bd97589e80b85a9fa2a4052f32c7d7
Tim Kye <Timothy.Kye@nike.com>
2021-10-06 09:46:54 -0700

    feat: include package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.0.0] - 2021-10-06
+### Added
+- keep `package.json` so that module `type` is included
+
 ## [0.6.1] - 2020-12-16
 ### Added
 `--all` to `npm ls` for npm 7
--- src/zipper.js
+++ src/zipper.js
@@ -10,10 +10,10 @@ const mkdirp = require('mkdirp')
 const globby = require('globby')
 
 module.exports = {
-  build
+  build,
 }
 
-function build ({ files, workingDir, outputPath, keepRoot }) {
+function build({ files, workingDir, outputPath, keepRoot }) {
   if (!files) {
     throw new Error('missing required parameter "files"')
   }
@@ -25,18 +25,18 @@ function build ({ files, workingDir, outputPath, keepRoot }) {
   let npm = exec('npm ls --prod --parseable --all', { cwd: workingDir })
   npm.stderr.pipe(process.stderr)
   npm.stdout.pipe(
-    concat(dependencies => {
+    concat((dependencies) => {
       dependencies = dependencies
         .split('\n')
         .slice(1) // skip the first entry, its the root
-        .filter(f => !!f) // remove empty
-        .map(f => path.relative(workingDir, f))
+        .filter((f) => !!f) // remove empty
+        .map((f) => path.relative(workingDir, f))
       archive({ workingDir, outputPath, dependencies, files, keepRoot })
     })
   )
 }
 
-function archive ({ files, workingDir, outputPath, dependencies, keepRoot }) {
+function archive({ files, workingDir, outputPath, dependencies, keepRoot }) {
   mkdirp.sync(path.dirname(path.join(workingDir, outputPath)))
   let output = fs.createWriteStream(path.join(workingDir, outputPath)) //, { flags: 'r+' })
   let archive = archiver('zip', { zlib: { level: 9 } })
@@ -61,7 +61,10 @@ function archive ({ files, workingDir, outputPath, dependencies, keepRoot }) {
 
   const paths = globby.sync(files, { cwd: workingDir, expandDirectories: true })
 
-  paths.forEach(file => {
+  // Always include the package.json so that the module type is included
+  archive.file(path.resolve(workingDir, 'package.json'), { name: 'package.json' })
+
+  paths.forEach((file) => {
     let stat = fs.statSync(path.resolve(workingDir, file))
     let archiveName = !keepRoot ? getFlattened(file, files) : file
     // console.log(file, flatName)
@@ -70,19 +73,17 @@ function archive ({ files, workingDir, outputPath, dependencies, keepRoot }) {
     } else if (stat.isFile()) {
       archive.file(file, { name: archiveName })
     } else {
-      throw new Error(
-        'Stat is not a file or directory, unable to append. ' + file
-      )
+      throw new Error('Stat is not a file or directory, unable to append. ' + file)
     }
   })
 
-  dependencies.forEach(dep => archive.directory(dep, dep))
+  dependencies.forEach((dep) => archive.directory(dep, dep))
   archive.finalize()
 }
 
-function getFlattened (file, files) {
+function getFlattened(file, files) {
   // console.log('check', file, files)
-  let match = files.find(f => file.startsWith(f))
+  let match = files.find((f) => file.startsWith(f))
   if (!match.endsWith('/')) match += '/'
   return file.replace(match, '')
 }
